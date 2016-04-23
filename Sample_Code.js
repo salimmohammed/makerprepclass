7636108c6bbe9c46bf3a990fd6ee31a3d1c3af9e
Salim Mohammed-class <salimmohammed@yahoo.com>
2016-04-22 20:18:56 -0700

    updated sample code file

--- Sample_Code.js
+++ Sample_Code.js
@@ -169,16 +169,21 @@ function add (x, y) {
 
 function scoreToGrade (x) {
 	if (x>100 || x<0) {
-		return "INVALID SCORE" }
+		return "INVALID SCORE"
+		}
 	else if (x>90) {
-		return "A"}
+		return "A"
+	}
 		else if (x>=80) {
-			return "B" }
-				else if (x>=70) {
-					return "C" }
-						else if (x>=60) {
-							return "D" }
-								else if (x<60) {
-									return "F"
-								}
-							}
+			return "B"
+		}
+			else if (x>=70) {
+				return "C"
+			}
+				else if (x>=60) {
+				return "D"
+				}
+					else if (x<60) {
+					return "F"
+					}
+}
